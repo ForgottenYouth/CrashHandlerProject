ec337d243ebb5bd18c320703d1bce379c8c8ebfa
shiwenliang <540658894@qq.com>
2020-09-18 15:19:30 +0800

    create tools file

---
+++ app/src/main/java/com/leon/crashhandlerproject/CrashHandler.kt
@@ -0,0 +1,18 @@
+/*
+* Copyright (C), 2015-2020, 物联云仓科技有限公司
+* FileName: CrashHandler
+* Author: shiwenliang
+* Date: 2020/9/18 15:18
+* Description: 异常捕获的工具
+*/
+package com.leon.crashhandlerproject
+
+class CrashHandler : Thread.UncaughtExceptionHandler {
+    override fun uncaughtException(t: Thread, e: Throwable) {
+
+        /*
+        * TODO 捕获异常处理
+        */
+    }
+
+}
